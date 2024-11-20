0d9e4bc856c67e3ca9b50fa3d6507700074f441c
Faiyan khan <144541895+faiyankhan@users.noreply.github.com>
2024-11-20 08:28:35 +0000

    onclick applied

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function openModel(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'flex';
+}
+
+function closeModel(){
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display = 'none';
+}
+
+function accountCreate(){
+    alert("Accout Created Successfully 🎉🎉🎉")
+}
